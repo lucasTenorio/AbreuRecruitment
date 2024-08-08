323413c38e3845c20e4f02eeae5e56fca7591b35
Lucas Tenório <lucas_tenorio@hotmail.com.br>
2024-08-08 18:11:00 -0300

    add dependency for angular material

--- VAArtGalleryWebApp/src/app/app.module.ts
+++ VAArtGalleryWebApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { importProvidersFrom, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,23 +6,41 @@ import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { ArtworkPortfolioComponent } from './artwork-portfolio/artwork-portfolio.component';
+import { ModalComponent } from './modal/modal.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     GalleryComponent,
-    ArtworkPortfolioComponent
+    ArtworkPortfolioComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
     MatIconModule,
-    HttpClientModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(MatNativeDateModule)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
