2f143639b7541baeb6bc090ed832c0ae2e6eafec
Lucas Tenório <lucas_tenorio@hotmail.com.br>
2024-08-08 13:11:21 -0300

    removing old queries

--- VARecruitmentWebAPI/Application/Queries/GetAllArtGalleriesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using VAArtGalleryWebAPI.Domain.Entities;
-
-namespace VAArtGalleryWebAPI.Application.Queries
-{
-    public class GetAllArtGalleriesQuery : IRequest<List<ArtGallery>>
-    {
-    }
-}
